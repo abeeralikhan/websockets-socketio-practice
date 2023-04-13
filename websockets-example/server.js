1b3d6165fe0d07150b53073190cc4aaf0aaf951c
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-04-13 07:52:50 +0500

    implement basic client server communication using WebSockets

--- websockets-example/server.js
+++ websockets-example/server.js
@@ -1,10 +1,31 @@
-const http = require("http");
-const websocket = require("ws");
+const { createServer } = require("http");
+const { WebSocketServer } = require("ws");
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
   res.end("Hello from HTTP server");
 });
 
-const wss = new websocket.WebSocketServer({ server });
+// wss --> Websocket Server
+const wss = new WebSocketServer({ server });
+
+wss.on("headers", (headers, req) => {
+  // This runs when the handshake is in process
+  // We can customize the headers
+  console.log(headers);
+});
+
+// Websocket Server (wss) --> Websocket (ws)
+// connection --> open
+wss.on("connection", (ws, req) => {
+  // This runs when the connection is established
+  // Handshake is completed
+  ws.send("Welcome to the server!!");
+
+  // Once the connection is established
+  // we can start listening to messages from the server
+  ws.on("message", (data) => {
+    console.log(data.toString());
+  });
+});
 
 server.listen(8000);
